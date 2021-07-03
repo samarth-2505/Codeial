60dfb027a7d8de70c5c3ab52c22e027ea17cf935
Samarth Chaube <18ucc135@lnmiit.ac.in>
2021-07-03 15:07:07 +0530

    Added feature of posting for the authenticated users

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,10 +1,25 @@
+const Post= require('../models/post');
+
+
 module.exports.home = function(req, res){
     //cookies come with the req 
     console.log(req.cookies);
     //we can change the cookie's key's value like this at the server side
     // res.cookie('user_id',25);
-    return res.render('home', {
-        title: "Home Page"
+    
+    // Post.find({},function(err,posts){
+    //     return res.render('home', {
+    //         title: "Home Page",
+    //         posts : posts            //This way, only the user's id will be accesible in the ejs file
+    //     });
+    // });
+    
+    //populate the user of each post 
+    Post.find({}).populate('user').exec(function(err,posts){
+        return res.render('home', {
+            title: "Home Page",
+            posts : posts
+        });
     });
 };
 
