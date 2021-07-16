a97e691cf1528014c76c3e37ebab56823f97e2a3
Samarth Chaube <18ucc135@lnmiit.ac.in>
2021-07-16 14:21:48 +0530

    Converted post and comments creation to AJAX requests

--- controllers/posts_controller.js
+++ controllers/posts_controller.js
@@ -8,6 +8,14 @@ module.exports.create= function(req,res){
         user : req.user._id
     },function(err,post){
         if(err){req.flash('error', 'Error in Creating Post !'); return res.redirect('back');}
+        if(req.xhr){
+          return res.status(200).json({
+            data : {
+              post : post
+            },
+            message : "Post Created !"
+          });
+        }
         req.flash('success', 'Post Published !');
         return res.redirect('back');
     });
@@ -21,6 +29,14 @@ module.exports.destroy = async function(req,res){
     if(post.user == req.user.id){
      post.remove();
      await Comment.deleteMany({post : req.params.id /* or post.id */});
+     if(req.xhr){
+       return res.status(200).json({
+        data : {
+          post_id : req.params.id
+        },
+        message : "Post Deleted !"
+       });
+     }
      req.flash('success', 'Post Deleted !');
      return res.redirect('back');
     }
