10709b2e0f74c957d402b47a606a50a8d43e2b9e
Samarth Chaube <18ucc135@lnmiit.ac.in>
2021-07-19 17:44:39 +0530

    Added API routes and controllers to the directory, added the function to delete a post using API request and sending back JSON response through API controller

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,6 @@
 const express = require('express');
-
 const router = express.Router();
+
 const homeController = require('../controllers/home_controller');
 
 console.log('router loaded');
@@ -10,6 +10,9 @@ router.get('/', homeController.home);
 router.use('/users', require('./users'));
 router.use('/posts',require('./posts'));
 router.use('/comments',require('./comments'));
+
+
+router.use('/api',require('./api'));
 
 // for any further routes, access from here
 // router.use('/routerName', require('./routerfile));
