26006fd8f092cab6001f207a952975009206e71f
Samarth Chaube <18ucc135@lnmiit.ac.in>
2021-07-23 20:46:03 +0530

    added the routes for google authentication and added the button for it on sign-in and sign-up page

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ const session=require('express-session');
 const passport=require('passport');
 const passportJWT = require('./config/passport-jwt-strategy');
 const passportLocal=require('./config/passport-local-strategy');
+const passportGoogle = require('./config/passport-google-oauth-2-strategy');
 const MongoStore = require('connect-mongo');
 const sassMiddleware= require('node-sass-middleware');
 const flash = require('connect-flash');
